be365f4bea84bc6ea6df69e03c6e45624c526a5a
muhammedovsardor2004-wq <muhammedovsardor2004@gmail.com>
2025-11-04 18:50:32 +0500

    Create less1.py

---
+++ lesson-1/homework/less1.py
@@ -0,0 +1,41 @@
+1)
+a=float( input("kavdrat tomonini kirit") )
+
+perimeter = a*4
+area = a*a
+
+print( perimeter)
+area
+
+2)
+
+import math
+
+d = float(input("deametr kirit"))
+deametr = math.pi*d
+
+print("deametr=",deametr) 
+
+
+3)
+a = float(input("birinchi qiymatni kirit"))
+b = float(input("ikkinchi qiymatni kirit"))
+
+mean = (a+b)/2
+
+print("o`rtacha qiymat" ,mean)
+
+4)
+
+a = float(input("birinchi qiymatni kirit"))
+b = float(input("ikkinchi qiymatni kirit"))
+
+total = a+b
+product = a*b
+square1 = a**2
+square2 = b**2
+
+print ("total",total)
+print ("product",product)
+print ("square1",square1)
+print ("square2",square2)
